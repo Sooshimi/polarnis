a4e6a4c42534597277b6adac277e3cfc4c477216
Sooshimi <59628017+Sooshimi@users.noreply.github.com>
2024-02-23 21:45:54 +0000

    Add movement state machine

--- scripts/characters/smek.gd
+++ scripts/characters/smek.gd
@@ -1,13 +1,25 @@
 extends CharacterBody2D
 
 var speed: int = 100
+var input_direction: Vector2
+
+@onready var animation_tree: AnimationTree = $AnimationTree
 
 func _process(_delta):
 	get_input()
+	play_animations()
 
 func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
+	input_direction = Input.get_vector("left", "right", "up", "down")
 	
 	velocity = input_direction * speed
 	
 	move_and_slide()
+
+func play_animations():
+	if input_direction == Vector2.ZERO:
+		animation_tree.get("parameters/playback").travel("idle")
+	else:
+		animation_tree.get("parameters/playback").travel("walk")
+		animation_tree.set("parameters/idle/blend_position", input_direction)
+		animation_tree.set("parameters/walk/blend_position", input_direction)
